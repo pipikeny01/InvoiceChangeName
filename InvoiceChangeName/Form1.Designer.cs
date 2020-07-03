d518a45fe1c31e0b95afe2489663d6e731835df7
kenny <pipikeny@gmail.com>
2020-07-03 13:47:37 +0800

    加入專案檔案。

---
+++ InvoiceChangeName/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace InvoiceChangeName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(596, 60);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(83, 43);
+            this.btnFolder.TabIndex = 0;
+            this.btnFolder.Text = "資料夾";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(48, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(507, 154);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(83, 43);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "轉換";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 284);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ InvoiceChangeName/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceChangeName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = @"C:\Users\admin\Documents\要重傳的發票\test";
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                var resultFolder = Path.Combine(textBox1.Text,"result");
+                if (!Directory.Exists(resultFolder))
+                    Directory.CreateDirectory(resultFolder);
+
+                var directoryInfo = new System.IO.DirectoryInfo(textBox1.Text);
+                foreach (var fileInfo in directoryInfo.GetFiles())
+                {
+                    if (fileInfo.Name.Contains("xml"))
+                    {
+                        var newName = fileInfo.Name.Substring(0, fileInfo.Name.IndexOf("l", StringComparison.Ordinal) + 1);
+                        fileInfo.CopyTo(Path.Combine(resultFolder, newName));
+                    }
+                }
+
+                MessageBox.Show("Ok");
+            }
+
+        }
+    }
+}
